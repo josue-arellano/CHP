da815cd826fe7c44f5a983fcdb67ccd3c3a34f49
Josue Arellano <arellajosue@protonmail.com>
2019-08-25 18:55:06 -0700

    Update course listing url and clean up file

--- server.js
+++ server.js
@@ -2,18 +2,36 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
+const semesters = [
+    'winter',
+    'spring',
+    'summer',
+    'fall'
+]
+var semesterIndex = 0
+var firstYear = 2019
+var courseListArray = new Array()
+var today = new Date()
 var bodyParser = require('body-parser')
 var request = require('request')
 var cheerio = require('cheerio')
 var schedule = require('node-schedule')
 var mongodb = require('mongodb')
 var ObjectID = mongodb.ObjectID
-
 var db
-const PORT = process.env.PORT || 5000
-const URLPrefix = 'http://205.154.255.98/academics/schedules/courselisting/spring2019-courselisting-n.htm'
 
-mongodb.MongoClient.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/test',{ useNewUrlParser: true }, function(err, client) {
+const PORT = process.env.PORT || 5000
+//The new uri is of the form:
+//http://205.154.255.98/academics/schedules/courselisting/spring2019-courselisting-n.htm
+const URLPrefix = 'http://205.154.255.98/academics/schedules/courselisting/'
+const URLPostfix = '-courselisting-n.htm'
+
+// This connects the app to the database
+mongodb.MongoClient.connect(process.env.MONGODB_URI
+                            || 'mongodb://localhost:27017/test',
+                            { useNewUrlParser: true },
+                            function(err, client) {
     if(err) {
         console.log(err)
         process.exit(1)
@@ -27,17 +45,16 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI || 'mongodb://localhost:2701
     })
 })
 
-const semesters = [
-    'winter',
-    'spring',
-    'summer',
-    'fall'
-]
-var semesterIndex = 0
-var year = 2019
-var courseListArray = new Array()
-var today = new Date()
+// This is used to get a consistent url throughout the program
+function createURL(semester, year) {
+    if(semesters.indexOf(semester) >= 0 && year >= firstYear ) {
+        return URLPrefix + semester + year + URLPostfix
+    } else {
+        return null
+    }
+}
 
+// Used to check if the url exists
 function checkRequest(url) {
     return new Promise((resolve, reject) => {
         request(url, (error, res, html) => {
@@ -50,13 +67,15 @@ function checkRequest(url) {
     })
 }
 
+
 var getSemester = (semester, year) => {
     let collections = []
     db.command("")
     return collections
 }
 
-function getRequest(url) {
+// Returns the html of the given url
+function fromURL(url) {
     return new Promise((resolve, reject) => {
         request(url, (error, res, html) => {
             if(!error && res.statusCode == 200) {
@@ -68,8 +87,8 @@ function getRequest(url) {
     })
 }
 
-var runMe = async (url, courseNumber) => {
-    var html = await getRequest(url)
+var getCourseListing = async (url, courseNumber) => {
+    var html = await fromURL(url)
                         .catch((err) => {
                             console.log(err)
                         })
@@ -106,55 +125,6 @@ var runMe = async (url, courseNumber) => {
     return course
 }
 
-schedule.scheduleJob({hour: 16, minute: 07}, async () => {
-    var semesterExists = true
-    var urlPrefix = 'http://elac.edu/academics/schedules/courselisting/class_list_'
-    var testYear = today.getFullYear()
-    var testURL
-    var selectedURL
-
-    while(semesterExists) {
-        testURL = urlPrefix + semesters[semesterIndex] + '_' + testYear + '.htm'
-        await checkRequest(testURL)
-            .then(() => {
-                selectedURL = testURL
-            }).catch((err) => {
-                semesterExists = false
-            })
-        if(semesterIndex === 3) {
-            testYear += 1
-            semesterIndex = 0
-        } else {
-            semesterIndex++
-        }
-    }
-    var $
-
-    request(selectedURL, (error, response, html) => {
-        if(!error && response.statusCode == 200) {
-            $ = cheerio.load(html)
-            $('tbody').find('tr').each(function(i, elem) {
-                var courseArray = []
-                $(this).find('td').each(function(j, elem) {
-                    courseArray.push($(this).text())
-                })
-                if(courseArray.length == 14) {
-                    var courseInfo = {
-                        courseName: courseArray[0].substring(1, courseArray[0].length-1),
-                        courseDesc: courseArray[1].substring(1, courseArray[1].length-1),
-                        courseNum: courseArray[2].substring(1, 6),
-                        daysOfWeek: courseArray[4].substring(1, courseArray[4].length-1),
-                        meetingTime: courseArray[5].substring(1, courseArray[5].length-1),
-                        meetingDates: courseArray[13].substring(1, 22),
-                    }
-                    courseListArray.push(courseInfo)
-                }
-            })
-        } else {
-            console.log("There was an error")
-        }
-    })
-})
 app.use(bodyParser.json())
 app.get('/api', async (req, res) => {
     if(!req.query.semester) {
@@ -177,8 +147,8 @@ app.get('/api', async (req, res) => {
     }
     const semester = req.query.semester
     const year = req.query.year
-    
-    const response = await runMe('http://205.154.255.98/academics/schedules/courselisting/'+ semester + '_' + year + '.htm', req.query.course)
+    let url = createURL(semester, year)
+    const response = await getCourseListing(url, req.query.course)
 
     console.log("done")
     console.log(response)
@@ -191,6 +161,7 @@ app.use(express.static(__dirname + '/dist/frontend'));
 app.get('/', function(req,res) {
     res.sendFile(path.join(__dirname+'/dist/frontend/index.html'));
 });
+
 app.get('/vsa', function(req,res) {
     res.sendFile(path.join(__dirname+'/dist/frontend/index.html'));
 });
@@ -199,16 +170,6 @@ app.get('/semester', function(req, res) {
     let semester = req.query.semester
     let year = req.query.year
     collections = getSemester(semester, year)
-    res.send({
-        "message": "ok",
-        "collections": collections
-    })
-})
-
-app.get('/test', function(req, res) {
-    let semester = req.query.semester
-    let year = req.query.year
-    collections = (semester, year)
     res.send({
         "message": "ok",
         "collections": collections
